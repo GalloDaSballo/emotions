cc0aa30977df5de29e6f142a7ddd69ce9d71d84b
Gallo Da Sballo <alessandro@hubmade.com>
2019-12-31 16:21:31 -0300

    Updates

---
+++ precache-manifest.4f971f7d6527780be23ab7063cb641cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe650374bd3f5ea6a6ee7208b124a029",
+    "url": "/emotions/index.html"
+  },
+  {
+    "revision": "b7af2b70b2419620a4af",
+    "url": "/emotions/static/css/main.c824bfd8.chunk.css"
+  },
+  {
+    "revision": "633aa344338130233c11",
+    "url": "/emotions/static/js/2.4884fd23.chunk.js"
+  },
+  {
+    "revision": "b7af2b70b2419620a4af",
+    "url": "/emotions/static/js/main.afd7269d.chunk.js"
+  },
+  {
+    "revision": "248b5a7635eea62b5639",
+    "url": "/emotions/static/js/runtime-main.367bc94f.js"
+  }
+]);
